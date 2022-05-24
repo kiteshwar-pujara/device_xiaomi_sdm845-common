Grill-Laux/device_oneplus_sdm845-common

sdm845-common: lights: Make notification led priority > battery led one
Change-Id: Ibece77fb3495f9add70668d8ca0e74a8fe66d83a

lights/Light.cpp
@@ -58,8 +58,8 @@ static int rgbToBrightness(const LightState& state) {
 Light::Light() {
     mLights.emplace(Type::ATTENTION, std::bind(&Light::handleRgb, this, std::placeholders::_1, 0));
     mLights.emplace(Type::BACKLIGHT, std::bind(&Light::handleBacklight, this, std::placeholders::_1));
-    mLights.emplace(Type::BATTERY, std::bind(&Light::handleRgb, this, std::placeholders::_1, 1));
-    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleRgb, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::BATTERY, std::bind(&Light::handleRgb, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleRgb, this, std::placeholders::_1, 1));
 }
 
 void Light::handleBacklight(const LightState& state) {

kiteshwar-pujara/device_xiaomi_sdm845-common

light/Light.cpp
@@ -64,8 +64,8 @@ static uint32_t getBrightness(const LightState& state) {
 
 Light::Light() {
     mLights.emplace(Type::ATTENTION, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 0));
-    mLights.emplace(Type::BATTERY, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 1));
-    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::BATTERY, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 2));
+    mLights.emplace(Type::NOTIFICATIONS, std::bind(&Light::handleWhiteLed, this, std::placeholders::_1, 1));
 }
 
 void Light::handleWhiteLed(const LightState& state, size_t index) {